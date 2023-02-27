324ad9917b434769c311b4e4907644d2bb1c2f7e
Edward Klesel <edwardklesel@gmail.com>
2023-02-27 17:23:31 +0000

    Add Find function to code

--- 2022/GO/tools/tools.go
+++ 2022/GO/tools/tools.go
@@ -29,3 +29,15 @@ func ReadInput(filename string) (content []string) {
 	return content
 
 }
+
+func Find(arr []string, str string) bool {
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == str {
+			return true
+		}
+	}
+
+	return false
+
+}
