2e418557358c6f5d64a78372f03bed1e16568a21
Edward Klesel <edwardklesel@gmail.com>
2023-02-27 16:08:10 +0000

    Upload day 2

---
+++ 2022/GO/Day 2/go.mod
@@ -0,0 +1,5 @@
+module day2
+
+go 1.20
+
+require github.com/edklesel/AdventOfCode/2022/GO/tools v0.0.0-20230227101555-c311029e97ec
---
+++ 2022/GO/Day 2/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	tools "github.com/edklesel/AdventOfCode/2022/GO/tools"
+)
+
+var filename string = "day2.txt"
+
+func find(arr []string, str string) bool {
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == str {
+			return true
+		}
+	}
+
+	return false
+
+}
+
+func main() {
+
+	input := tools.ReadInput(filename)
+
+	var score int = 0
+
+	var scores = make(map[string]int)
+	scores["X"] = 1 //Rock
+	scores["Y"] = 2 //Paper
+	scores["Z"] = 3 //Scissors
+
+	var opponent_move string
+	var your_move string
+	for i := 0; i < len(input); i++ {
+
+		opponent_move = strings.Split(input[i], " ")[0]
+		your_move = strings.Split(input[i], " ")[1]
+
+		// Win scenarios
+		var win = []string{"AY", "BZ", "CX"}
+		var lose = []string{"AZ", "BX", "CY"}
+		var tie = []string{"AX", "BY", "CZ"}
+
+		// Win scenrarios
+		score += scores[your_move]
+		if find(win, opponent_move+your_move) {
+			// fmt.Println("You Win")
+			score += 6
+		} else if find(lose, opponent_move+your_move) {
+			// fmt.Println("You lose")
+		} else if find(tie, opponent_move+your_move) {
+			// fmt.Println("Tied game")
+			score += 3
+		}
+
+	}
+
+	fmt.Println("Part 1")
+	fmt.Printf("Your final score: %d\n", score)
+
+	var outcome string
+
+	score = 0
+	scores["A"] = 1 //Rock
+	scores["B"] = 2 //Paper
+	scores["C"] = 3 //Scissors
+
+	for i := 0; i < len(input); i++ {
+
+		opponent_move = strings.Split(input[i], " ")[0]
+		outcome = strings.Split(input[i], " ")[1]
+
+		switch outcome {
+		case "X": //Lose
+			switch opponent_move {
+			case "A":
+				score += scores["Z"]
+			case "B":
+				score += scores["X"]
+			case "C":
+				score += scores["Y"]
+			}
+
+		case "Y": //Tie
+			score += 3 + scores[opponent_move]
+
+		case "Z": //Win
+			score += 6
+			switch opponent_move {
+			case "A":
+				score += scores["Y"]
+			case "B":
+				score += scores["Z"]
+			case "C":
+				score += scores["X"]
+			}
+
+		}
+
+	}
+
+	fmt.Println("")
+	fmt.Println("Part 2")
+	fmt.Printf("Your final score: %d\n", score)
+
+}
